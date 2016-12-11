57f990bf369712047296848d9e17f83aabd3ee75
Thomas Sanders <sanders.td@gmail.com>
2016-12-11 15:25:21 -0500

    babelrc + gulpfile.babel

---
+++ gulpfile.babel.js
@@ -0,0 +1,8 @@
+import babel from 'gulp-babel';
+import gulp from 'gulp';
+import gulpSequence from 'gulp-sequence';
+import nodemon from 'gulp-nodemon';
+
+gulp.task('test', () =>
+  console.log('test'),
+);
